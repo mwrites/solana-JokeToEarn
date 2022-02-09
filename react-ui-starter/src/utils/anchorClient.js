aebc6cd93c3f3ff0a6410b40f6fe906bf1629aff
matanwrites <20499416+matanwrites@users.noreply.github.com>
2022-02-10 02:08:16 +0800

    test

--- react-ui-starter/src/utils/anchorClient.js
+++ react-ui-starter/src/utils/anchorClient.js
@@ -1,4 +1,4 @@
-import { Connection } from '@solana/web3.js';
+import Connection from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import { programAddress, connectionsOptions } from './config';
 
